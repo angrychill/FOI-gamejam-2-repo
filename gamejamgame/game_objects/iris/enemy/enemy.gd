86b53dc95e9ed448ab8c20aebfe393b115c31ece
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-29 19:43:10 +0100

    visible collision shapes

--- gamejamgame/game_objects/iris/enemy/enemy.gd
+++ gamejamgame/game_objects/iris/enemy/enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 class_name Enemy
-@onready var enemy_debug_label: Label = $EnemyDebugLabel
+@onready var enemy_debug_label: Label3D = $EnemyDebugLabel
+
 
 @export var enemy_data : EnemyData
 @export var sprite_3d : Sprite3D
@@ -20,4 +21,6 @@ func _ready() -> void:
 	max_enemy_health = enemy_data.health
 	current_enemy_health = enemy_data.health
 	enemy_speed = enemy_data.move_speed
-	
+
+func _physics_process(delta: float) -> void:
+	enemy_debug_label.text = str(current_enemy_health)
