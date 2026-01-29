ee39ed7a79d61c869e566a84f6819b8ccba96f25
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-29 22:02:07 +0100

    lantern start

--- gamejamgame/game_objects/iris/lantern/lantern.gd
+++ gamejamgame/game_objects/iris/lantern/lantern.gd
@@ -1,11 +1,11 @@
 extends Node3D
 class_name Lantern
+@onready var lantern_debug_label: Label = $LanternDebugLabel
 
-@export var shooting_rate_timer : Timer
 @export var shooting_decay_timer : Timer
 @export var max_shooting_rate : float = 100
-@export var shooting_rate_increment : float = 0.2
-@export var shooting_decay_rate : float = 0.5
+@export var shooting_rate_increment : float = 0.1
+@export var shooting_decay_rate : float = 0.25
 
 var _current_shooting_rate := 0.0
 
@@ -14,21 +14,33 @@ var current_shooting_rate : float:
 		return _current_shooting_rate
 	set(value):
 		_current_shooting_rate = clamp(value, 0.0, max_shooting_rate)
-		update_shooting_timer()
+
+var fire_accumulator : float = 0.0
 
 func _ready() -> void:
-	current_shooting_rate = 0.1
+	current_shooting_rate = 0.0
 	shooting_decay_timer.wait_time = shooting_decay_rate
-	shooting_rate_timer.timeout.connect(_on_shooting_rate_timer_timeout)
+
 	shooting_decay_timer.timeout.connect(_on_shooting_decay_timer_timeout)
 
 func _physics_process(delta: float) -> void:
-	#print_debug("shooting rate: ", current_shooting_rate)
-	pass
+	
+	lantern_debug_label.text = str(current_shooting_rate)
+	
+	if current_shooting_rate <= 0.0:
+		fire_accumulator = 0.0
+		return
+	
+	fire_accumulator += delta * current_shooting_rate
+	
+	while fire_accumulator >= 1.0:
+		fire_accumulator -= 1.0
+		fire_pulse()
+	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("scroll_down"):
-		print_debug("setting shooting rate")
+
 		current_shooting_rate += shooting_rate_increment
 		
 
@@ -36,17 +48,9 @@ func _on_shooting_decay_timer_timeout() -> void:
 	current_shooting_rate -= shooting_rate_increment
 
 
-func _on_shooting_rate_timer_timeout() -> void:
-	# fire pulse
-	print_debug("firing a pulse")
+func fire_pulse() -> void:
+	print("firing a pulse")
 
-func update_shooting_timer() ->void:
-	if current_shooting_rate <= 0.0:
-		shooting_rate_timer.stop()
-	else:
-		shooting_rate_timer.wait_time = 1.0 / current_shooting_rate
-		if shooting_rate_timer.is_stopped():
-			shooting_rate_timer.start()
-		
+
 		
 		
