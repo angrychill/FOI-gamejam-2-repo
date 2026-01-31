45a478c6b38f96c7ebf6094d87db5bb9585295ab
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-31 08:08:00 +0100

    restart level + kill plane

---
+++ gamejamgame/game_objects/iris/level/kill_plane.gd
@@ -0,0 +1,13 @@
+extends Area3D
+class_name KillPlane
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body : Node3D) -> void:
+	if body is FPSPlayer:
+		# restart
+		print_debug("restart")
+		var level : Level = get_tree().get_first_node_in_group("level")
+		if level:
+			level.restart_level()
