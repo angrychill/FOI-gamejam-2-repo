cbb34501d78c977adda672d1294d34023b2d5bf8
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-31 11:37:23 +0100

    basic deatah screen

---
+++ gamejamgame/assets/death_screen.gd
@@ -0,0 +1,14 @@
+extends Control
+@export var da_button : Button
+@export var ne_button : Button
+
+
+func _on_da_button_pressed() -> void:
+	# restart
+	pass
+	
+
+
+func _on_ne_button_pressed() -> void:
+	# switch to level select
+	pass
