679158fc3484ce6f6d06a97f0f3b3abe81c38a2b
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-31 09:01:24 +0100

    lantenr sfx

--- gamejamgame/game_objects/iris/weapon/weapon.gd
+++ gamejamgame/game_objects/iris/weapon/weapon.gd
@@ -2,7 +2,32 @@
 extends Node3D
 class_name Weapon
 
+@export var carry_sound_effects : Array[AudioStream]
+@export var attack_sound_effects : Array[AudioStream]
 @export var damage : int
+
+@export var audio_player : AudioStreamPlayer3D
+
 
 func attack() -> void:
 	pass
+
+func play_carry_sound_effect() -> void:
+	audio_player.bus = &"SFX"
+	if carry_sound_effects:
+		var rand_sfx : AudioStream = carry_sound_effects.pick_random()
+		audio_player.stream = rand_sfx
+		audio_player.pitch_scale = randf_range(-0.5, 1.5)
+		audio_player.play()
+
+func play_attack_sound_effect(pitch_scale : float = -1) -> void:
+	audio_player.bus = &"SFX"
+	if attack_sound_effects:
+		var rand_sfx : AudioStream = attack_sound_effects.pick_random()
+		audio_player.stream = rand_sfx
+		if pitch_scale < 0:
+			audio_player.pitch_scale = randf_range(-0.5, 1.5)
+		else:
+			audio_player.pitch_scale = pitch_scale
+		audio_player.play()
+		
