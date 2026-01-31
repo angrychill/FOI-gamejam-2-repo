83ba6a1b197b8d74cbd11957e54058ba80970eca
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-31 10:50:30 +0100

    intro level aaa

--- gamejamgame/game_objects/iris/global_data.gd
+++ gamejamgame/game_objects/iris/global_data.gd
@@ -17,3 +17,8 @@ func get_player_position() -> Vector3:
 	
 	else:
 		return player.global_position
+
+func switch_to_level(switch_level : PackedScene) -> void:
+	var level : Level = get_tree().get_first_node_in_group("level")
+	level.next_level = switch_level
+	level.on_level_complete()
