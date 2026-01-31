3e06d209c2509ed7d0aeddb18c09190c6822d7af
angrychill <93102183+angrychill@users.noreply.github.com>
2026-01-31 09:01:24 +0100

    lantenr sfx

--- gamejamgame/game_objects/iris/lantern/lantern.gd
+++ gamejamgame/game_objects/iris/lantern/lantern.gd
@@ -21,6 +21,7 @@ var current_shooting_rate : float: # = strength
 var fire_accumulator : float = 0.0
 
 func _ready() -> void:
+
 	trail.set_mode_manual()
 	current_shooting_rate = 0.0
 	shooting_decay_timer.wait_time = shooting_decay_rate
@@ -44,7 +45,7 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("scroll_down"):
-
+		play_carry_sound_effect()
 		current_shooting_rate += shooting_rate_increment
 		
 
@@ -59,6 +60,9 @@ func fire_pulse() -> void:
 	trail.emit_now(
 		trail.shape.radius - trail.shape.radius/2 + current_shooting_rate
 	)
+	
+
+	play_attack_sound_effect(remap(current_shooting_rate, max_shooting_rate, 0, 1.5, 0.5))
 
 	var overlaps := lantern_area.get_overlapping_bodies()
 	for node: Node3D in overlaps:
